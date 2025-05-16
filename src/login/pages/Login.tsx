926aae0e96401a5713be1b72050386a74354f2bd
Eden Wolff <e.wolff@reply.com>
2025-05-16 14:16:43 +0100

    test

--- src/login/pages/Login.tsx
+++ src/login/pages/Login.tsx
@@ -17,9 +17,7 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
     });
 
     const { social, realm, url, usernameHidden, login, auth, messagesPerField } = kcContext;
-
     const { msg, msgStr } = i18n;
-
     const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(false);
 
     return (
